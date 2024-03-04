4b04723c1e51341f172c1aa2f31795cea2adabb1
Pavel Epanov <pavelepanov@MacBook-Air-Pavel.local>
2024-03-04 19:29:32 +0500

    add 3.6.2

--- march_4/home.py
+++ march_4/home.py
@@ -1,11 +1,33 @@
-a = int(input())
-c = []
-mysum = 0
-for i in range(a):
-    r = list(map(int, input().split()[1:]))
-    c.append(r)
-n = int(input())
-for j in c:
-    mysum += j[n-1]
+def ans361():
+    a = int(input())
+    c = []
+    mysum = 0
+    for i in range(a):
+        r = list(map(int, input().split()[1:]))
+        c.append(r)
+    n = int(input())
+    for j in c:
+        mysum += j[n-1]
 
-print(int(mysum/a))
+    print(int(mysum/a))
+
+def ans362():
+    categories = {"отлично": [8, 9, 10],
+    "хорошо": [5, 6, 7],
+    "удовлетворительно": [3, 4],
+    "неудовлетворительно": [0, 1, 2]}
+
+    a = int(input())
+    c = []
+    mysum = 0
+    for i in range(a):
+        r = list(map(int, input().split()[1:]))
+        c.append(r)
+    n = int(input())
+    for j in c:
+        mysum += j[n-1]
+
+    sred = int(mysum/a)
+    for key, value in categories.items():
+        if sred in value:
+            print(key)
